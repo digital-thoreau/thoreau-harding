9a2c394a8814afb52cd42ac5d7d9bca1fd221dcd
Christian Wach <needle@haystack.co.uk>
2024-10-19 13:36:43 +0100

    Update plugin header

--- thoreau-harding.php
+++ thoreau-harding.php
@@ -1,13 +1,34 @@
 <?php
 /**
- * Plugin Name: Thoreau Harding
- * Plugin URI: https://github.com/digital-thoreau/thoreau-harding
- * Description: Better handling of Walter Harding comments on the Digital Thoreau Website.
- * Author: Christian Wach
- * Version: 1.0
- * Author URI: https://haystack.co.uk
+ * Thoreau Harding
+ *
+ * Plugin Name:       Thoreau Harding
+ * Description:       Better handling of Walter Harding comments on the Digital Thoreau Website.
+ * Plugin URI:        https://github.com/digital-thoreau/thoreau-harding
+ * GitHub Plugin URI: https://github.com/digital-thoreau/thoreau-harding
+ * Version:           1.0
+ * Author:            Christian Wach
+ * Author URI:        https://haystack.co.uk
+ * License:           GPLv2 or later
+ * License URI:       https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Requires at least: 5.7
+ * Requires PHP:      7.4
+ * Text Domain:       commentpress-jstor
+ * Domain Path:       /languages
  *
  * @package Thoreau_Harding
+ * @link    https://github.com/digital-thoreau/thoreau-harding
+ * @license GPL v2 or later
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 // Exit if accessed directly.
